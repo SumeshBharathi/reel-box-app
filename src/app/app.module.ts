fb6a105ce6842060ced895bdceaf0b750a40d700
Sumesh <sumeshbharathi@gmail.com>
2020-04-29 15:38:32 +0530

    Text box added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NewuserComponent } from './newuser/newuser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NewuserComponent
   ],
   imports: [
     BrowserModule,
